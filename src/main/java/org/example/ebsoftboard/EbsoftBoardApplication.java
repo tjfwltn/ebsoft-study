c50a710e91785401c22f0f238a430a213cda4109
tjfwltn <tjfwltn1234@gmail.com>
2025-05-17 17:37:51 +0900

    fisrt commit

---
+++ src/main/java/org/example/ebsoftboard/EbsoftBoardApplication.java
@@ -0,0 +1,13 @@
+package org.example.ebsoftboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbsoftBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbsoftBoardApplication.class, args);
+    }
+
+}
